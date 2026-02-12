c0c62f90b1826e9de653f01b43239e78473697fd
mikkee888 <watcharapongruttum@gmail.com>
2026-02-12 12:42:30 +0700

    asd65as4d65asd2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :media, class_name: "Media", dependent: :destroy
+
   has_many :creators, dependent: :destroy
   has_many :audit_logs, dependent: :destroy
-  has_many :assets, dependent: :destroy
+  has_many :assets, dependent: :delete_all
+  has_many :media,  dependent: :delete_all
+
 
   validates :username, presence: true, uniqueness: true
   validates :email, uniqueness: true, allow_nil: true
