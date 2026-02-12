74739d136bd11e112023f40e0490c452cce3baf2
mikkee888 <watcharapongruttum@gmail.com>
2026-02-12 12:05:21 +0700

    asd65as4d65asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
-# app/models/user.rb
 class User < ApplicationRecord
   has_secure_password
 
-  
-
-  
-
+  has_many :media, class_name: "Media", dependent: :destroy
   has_many :creators, dependent: :destroy
   has_many :audit_logs, dependent: :destroy
-  has_many :assets, dependent: :nullify
-  # หรือ :destroy ถ้าอยากลบ asset ตาม user
+  has_many :assets, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   validates :email, uniqueness: true, allow_nil: true
